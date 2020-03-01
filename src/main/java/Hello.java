3c83e86b2b1355c36616489681d1ecc2a51c50d6
Piotr Kukla <ptrkukla148@gmail.com>
2020-03-01 12:26:43 +0100

    pozdro

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * ... comment class...
+ *
+ * @author ptrkukla148@gmail.com
+ * @since 01.03.2020
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
