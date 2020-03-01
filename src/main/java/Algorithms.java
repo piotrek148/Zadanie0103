272ca8856c38c4e47d616292fbd28114c3f1a1a4
Dagmara <dagmarakitkowska@gmail.com>
2020-03-01 13:39:12 +0100

    testy

---
+++ src/main/java/Algorithms.java
@@ -0,0 +1,8 @@
+/**
+ * ...comment class...
+ *
+ * @author dagmarakitkowska@gmail.com
+ * @since 01.03.2020
+ */
+final class Algorithms {
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * ...comment class...
+ *
+ * @author dagmarakitkowska@gmail.com
+ * @since 01.03.2020
+ */
+final class Test {
+}
