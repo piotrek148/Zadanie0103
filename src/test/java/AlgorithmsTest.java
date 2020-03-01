891add24cc3e622d0fb9ed5a36c65dd090aa08e2
Pawel <hyzy1988@o2.pl>
2020-03-01 15:42:09 +0100

    pleasie

--- src/test/java/AlgorithmsTest.java
+++ src/test/java/AlgorithmsTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -13,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class AlgorithmsTest {
 
     private Algorithms algorithms;
+
 
     @BeforeEach
     public void setup(){
@@ -29,5 +33,12 @@ class AlgorithmsTest {
         assertEquals(result, 5);
     }
 
+    @Test
+    public void testsumOf() {
+        List<Long> dane = List.of(1l,2l,3l,4l,5l,6l);
+        Optional<Long> result = algorithms.sumOf(dane);
+        Long aLong = result.get();
+        assertEquals(aLong,21);
+    }
 
 }
